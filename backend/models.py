1e5ae7fbbeec352feb074ee0afd9ca965d728746
Minjae Lee <mjlee041@gmail.com>
2026-04-01 10:48:45 -0400

    Added isActive column in deploys table

--- backend/models.py
+++ backend/models.py
@@ -144,6 +144,7 @@ class Deploy(Base):
     device_firmware_id = Column(Integer, primary_key=True)
 
     timestamp = Column(DateTime, default=datetime.utcnow)
+    isActive = Column(Boolean, default=False)
 
     __table_args__ = (
         ForeignKeyConstraint(
