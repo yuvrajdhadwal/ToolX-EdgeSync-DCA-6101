cc14f67e5c4b672711874623416959687b911e7b
Minjae Lee <mjlee041@gmail.com>
2026-04-09 23:39:48 -0400

    Made a python file that handles updating the database based on the message from the device

---
+++ backend/install_status.py
@@ -0,0 +1,31 @@
+from database import SessionLocal
+from models import Deploy
+
+def update_install_status(device_id: str, body: str):
+    if "Accepted" in body:
+        accepted = True
+    elif "Rejected" in body:
+        accepted = False
+    else:
+        return
+
+    db = SessionLocal()
+    try:
+        deploy = db.query(Deploy).filter(
+            Deploy.device_serial == device_id,
+            Deploy.isActive == True,
+            Deploy.isAccepted == None
+        ).first()
+
+        if not deploy:
+            print(f"[{device_id}] No active deployment found")
+            return
+
+        deploy.isAccepted = accepted
+        db.commit()
+
+    except Exception as e:
+        db.rollback()
+        print(f"[{device_id}] DB error: {e}")
+    finally:
+        db.close()
