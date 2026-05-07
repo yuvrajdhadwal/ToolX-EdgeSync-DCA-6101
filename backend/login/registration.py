b670019f292fac14148e2d7778d2dd1af351274e
Christian-Vela <vela.christian.s@gmail.com>
2026-05-06 23:05:47 -0400

    superuser can make superuser

--- backend/login/registration.py
+++ backend/login/registration.py
@@ -85,11 +85,5 @@ def register_user(user: UserType, db: Session, authorization: str | None = None)
             detail="Only a super user can register a user",
         )
 
-    if user.role == UserRole.super_user:
-        raise HTTPException(
-            status_code=403,
-            detail="Only one super user can exist",
-        )
-
     return create_user(db=db, user=user)
 
